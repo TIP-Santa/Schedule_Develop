c01927c924398c79f83092610029a12af08decef
TIP_Santa <bhb89@naver.com>
2024-10-08 11:01:11 +0900

    init commit

---
+++ src/main/groovy/com/sparta/schedule_develop/ScheduleDevelopApplication.groovy
@@ -0,0 +1,13 @@
+package com.sparta.schedule_develop
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ScheduleDevelopApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ScheduleDevelopApplication, args)
+    }
+
+}
