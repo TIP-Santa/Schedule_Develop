8c57deb52d7f9824bc21493106d00f8ab9399bda
TIP_Santa <bhb89@naver.com>
2024-10-16 17:55:10 +0900

    member, comments responseDto에 createdDateTime, modifiedDateTime 추가

--- src/main/java/com/sparta/schedule/dto/comments/CommentsResponseDto.java
+++ src/main/java/com/sparta/schedule/dto/comments/CommentsResponseDto.java
@@ -3,17 +3,23 @@ package com.sparta.schedule.dto.comments;
 import com.sparta.schedule.entity.Comments;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class CommentsResponseDto {
     private Long scheduleKey;
     private Long commentsKey;
     private String commenter;
     private String comments;
+    private LocalDateTime createdDateTime;
+    private LocalDateTime modifiedDateTime;
 
     public CommentsResponseDto(Comments comments) {
         this.scheduleKey = comments.getSchedule().getScheduleKey();
         this.commentsKey = comments.getCommentsKey();
         this.commenter = comments.getCommenter();
         this.comments = comments.getComments();
+        this.createdDateTime = comments.getCreatedDateTime();
+        this.modifiedDateTime = comments.getModifiedDateTime();
     }
 }
--- src/main/java/com/sparta/schedule/dto/member/MemberResponseDto.java
+++ src/main/java/com/sparta/schedule/dto/member/MemberResponseDto.java
@@ -3,15 +3,21 @@ package com.sparta.schedule.dto.member;
 import com.sparta.schedule.entity.Member;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class MemberResponseDto {
     private Long userKey;
     private String userName;
     private String userEmail;
+    private LocalDateTime createdDateTime;
+    private LocalDateTime modifiedDateTime;
 
     public MemberResponseDto(Member savedMember) {
         this.userKey = savedMember.getUserKey();
         this.userName = savedMember.getUserName();
         this.userEmail = savedMember.getUserEmail();
+        this.createdDateTime = savedMember.getCreatedDateTime();
+        this.modifiedDateTime = savedMember.getModifiedDateTime();
     }
 }
